38f7cd1c91c20b99cff1b15af1f5a47c30988303
Manuel J. González <majegonro@gmail.com>
2016-04-03 13:54:40 +0200

    Nivel 2

---
+++ Assets/TweenColor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweenColor : MonoBehaviour {
+
+    public Renderer thisRenderer;
+    public Color to = Color.clear;
+    public float tweenDuration = 3;
+
+
+    public void Play()
+    {
+        Hashtable tweenParams = new Hashtable();
+        tweenParams.Add("from", thisRenderer.material.color);
+        tweenParams.Add("to", to);
+        tweenParams.Add("time", tweenDuration);
+        tweenParams.Add("looptype", iTween.LoopType.none);
+        tweenParams.Add("onupdate", "OnColorUpdate");
+
+        iTween.ValueTo(gameObject, tweenParams);
+    }
+
+
+    private void OnColorUpdate(Color color)
+    {
+        thisRenderer.material.color = color;
+    }
+}
